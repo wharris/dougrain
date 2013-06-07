934fd3e8ccc828e53e052d59bd573e3135af570d
Will Harris <will@greatlibrary.net>
2013-06-07 09:49:03 +0100

    Memoize CURIE expansions.

--- dougrain/curie.py
+++ dougrain/curie.py
@@ -2,8 +2,18 @@
 # See the file license.txt for copying permission.
 
 import urlparse
+from random import choice
 
 class CurieCollection(dict):
+    def __init__(self, expansions_cache={}):
+        # The default value of expansions_cache is not a mistake. The
+        # dictionary is deliberately shared because the mapping between
+        # (template, rel) and expansion is valid everywhere.
+        super(CurieCollection, self).__init__()
+        self.expansions_cache = expansions_cache
+
+    EXPANSIONS_CACHE_LEN_LIMIT = 128
+
     def expand(self, link):
         terms = link.split(':')
 
@@ -15,4 +25,17 @@ class CurieCollection(dict):
         if key not in self:
             return link
 
-        return self[key].url(rel=value)
+        template = self[key]
+        memo_key = template.href, value
+
+        if memo_key in self.expansions_cache:
+            return self.expansions_cache[memo_key]
+
+        # Prevent unbounded growth of the shared cache.
+        if len(self.expansions_cache) >= self.EXPANSIONS_CACHE_LEN_LIMIT:
+            del self.expansions_cache[choice(self.expansions_cache.keys())]
+
+        result = self[key].url(rel=value)
+        self.expansions_cache[memo_key] = result
+
+        return result
