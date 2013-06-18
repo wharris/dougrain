538ee833200f1a0dc85252070ddc0079f07fe1a9
Will Harris <will@greatlibrary.net>
2013-06-18 09:34:33 +0100

    Add multiple links.

--- dougrain/builder.py
+++ dougrain/builder.py
@@ -11,6 +11,18 @@ class Builder(object):
     def as_object(self):
         return self.o
 
-    def add_link(self, rel, href):
-        self.o['_links'][rel] = {'href': href}
+    def add_link(self, rel, href, **kwargs):
+        new_link = dict(href=href, **kwargs)
+
+        if not rel in self.o['_links']:
+            self.o['_links'][rel] = new_link
+            return
+
+        existing_link = self.o['_links'][rel]
+        if isinstance(existing_link, list):
+            self.o['_links'][rel].append(new_link)
+            return
+
+        self.o['_links'][rel] = [existing_link, new_link]
+
 
--- test/test_builder.py
+++ test/test_builder.py
@@ -11,6 +11,11 @@ class BuilderTests(unittest.TestCase):
                                                self._testMethodName)
         self.builder = Builder(self.uri)
 
+    def testStartsWithNoProperties(self):
+        doc = Document.from_object(self.builder.as_object(),
+                                   base_uri="http://localhost")
+        self.assertEqual(doc.properties.keys(), [])
+
     def testSetNewProperty(self):
         self.builder.set_property('spam', "foo")
 
@@ -41,6 +46,49 @@ class BuilderTests(unittest.TestCase):
         
         item_link = doc.links['item']
         self.assertEqual(item_link.url(), doc.link("/items/1").url())
+
+    def testAddLinkWithOptionalProperties(self):
+        self.builder.add_link('item', "/items/{item_id}",
+                              name='name',
+                              title='Title',
+                              type='application/hal+json',
+                              profile='/profiles/item',
+                              hreflang='en',
+                              deprecation='/deprecation/item',
+                              templated=True)
+
+        doc = Document.from_object(self.builder.as_object(),
+                                   base_uri="http://localhost")
+        item_link = doc.links['item']
+        self.assertEqual(item_link.url(item_id='1'), "http://localhost/items/1")
+        self.assertEqual(item_link.name, 'name')
+        self.assertEqual(item_link.title, 'Title')
+        self.assertEqual(item_link.type, 'application/hal+json')
+        self.assertEqual(item_link.profile, '/profiles/item')
+        self.assertEqual(item_link.hreflang, 'en')
+        self.assertEqual(item_link.deprecation, '/deprecation/item')
+        self.assertTrue(item_link.is_templated)
+
+    def testAddSecondLink(self):
+        self.builder.add_link('item', '/items/1', name='first')
+        self.builder.add_link('item', '/items/2', name='second')
+
+        doc = Document.from_object(self.builder.as_object(),
+                                   base_uri="http://localhost")
+        item_links = doc.links['item']
+        self.assertSequenceEqual([item_link.name for item_link in item_links],
+                                 ['first', 'second'])
+
+    def testAddThirdLink(self):
+        self.builder.add_link('item', '/items/1', name='first')
+        self.builder.add_link('item', '/items/2', name='second')
+        self.builder.add_link('item', '/items/3', name='third')
+
+        doc = Document.from_object(self.builder.as_object(),
+                                   base_uri="http://localhost")
+        item_links = doc.links['item']
+        self.assertSequenceEqual([item_link.name for item_link in item_links],
+                                 ['first', 'second', 'third'])
 
 
 if __name__ == '__main__':
