d101a3966526f49346b2c89dbb684003ee347e52
Will Harris <will@greatlibrary.net>
2013-07-04 12:59:06 +0100

    Claim to support Python 3.2.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Operating System :: POSIX',
         'Topic :: Internet :: WWW/HTTP',
